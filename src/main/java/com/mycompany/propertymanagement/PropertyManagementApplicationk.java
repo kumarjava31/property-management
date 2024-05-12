6259b5e1bc2b26aeb34867889d77077fb5e39182
kumarjava31 <kpanchal.student@wust.edu>
2024-05-12 11:09:32 -0400

    done

---
+++ src/main/java/com/mycompany/propertymanagement/PropertyManagementApplicationk.java
@@ -0,0 +1,13 @@
+package com.mycompany.propertymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementApplicationk {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagementApplicationk.class, args);
+	}
+
+}
---
+++ src/main/java/com/mycompany/propertymanagement/entity/PropertyEntity.java
@@ -0,0 +1,28 @@
+package com.mycompany.propertymanagement.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="PROPERTY_TABLE")
+@Getter
+@Setter
+@NoArgsConstructor
+
+
+
+public class PropertyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "PROPERTY_TITLE", nullable = false, length = 50)
+    private String title;
+    private String description;
+
+    private Double price;
+    private String address;
+}
